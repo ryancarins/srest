80d9be597a359bde2e798cb32cb29455e03bde33
Ryan Carins <carinsryan@gmail.com>
2024-02-24 00:38:35 +1100

    Add POST/PUT

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,48 @@
 use std::io::Read;
+use reqwest::blocking::Response;
+use reqwest::blocking::Client;
 use serde_json::Value;
 use slint::SharedString;
 slint::include_modules!();
+
+fn handle_request(method: &str, query: &str, body: String) -> String {
+    let mut result: Result<Response, reqwest::Error>;
+    let client = Client::new();
+    match method {
+        "GET" => {
+            result = client.get(query).send();
+        },
+        "POST" => {
+            result = client.post(query).body(body).header("Content-Type", "application/json").send();
+        },
+        "PUT" => {
+            result = client.put(query).body(body).send();
+        },
+        _ => {panic!()}
+    }
+
+    if let Err(err) = result {
+        return err.to_string();
+    }
+    let mut res = result.unwrap();
+
+    let mut body = String::new();
+    println!("{}", method);
+    if res.read_to_string(&mut body).is_err() {
+        return "Failed to load result to string".to_string();
+    }
+    let json: Result<Value, serde_json::Error> = serde_json::from_str(body.as_str());
+    if json.is_err() {
+        return body;
+    }
+    let pretty = serde_json::to_string_pretty(&json.unwrap());
+
+    if pretty.is_err() {
+        return body;
+    }
+
+    pretty.unwrap()
+}
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
@@ -10,11 +51,10 @@ fn main() -> Result<(), slint::PlatformError> {
         let ui_handle = ui.as_weak();
         move || {
             let ui = ui_handle.unwrap();
-            let mut res = reqwest::blocking::get(ui.get_request().as_str()).unwrap();
-            let mut body = String::new();
-            res.read_to_string(&mut body).unwrap();
-            let json: Value = serde_json::from_str(body.as_str()).unwrap();
-            body = serde_json::to_string_pretty(&json).unwrap();
+            let method = ui.get_method();
+            let query = ui.get_request();
+            let body = ui.get_body();
+            let body = handle_request(&method, &query, body.to_string());
             ui.set_result(SharedString::from(body));
         }
     });
@@ -24,6 +64,22 @@ fn main() -> Result<(), slint::PlatformError> {
         move |text| {
             let ui = ui_handle.unwrap();
             ui.set_request(text);
+        }
+    });
+
+    ui.on_method_updated({
+        let ui_handle = ui.as_weak();
+        move |text| {
+            let ui = ui_handle.unwrap();
+            ui.set_method(text);
+        }
+    });
+
+    ui.on_body_updated({
+        let ui_handle = ui.as_weak();
+        move |text| {
+            let ui = ui_handle.unwrap();
+            ui.set_body(text);
         }
     });
 
